92546bb96428f61915da38877c62ce2437459acc
Dhaarani Singaravel <dhaaranisuriya2605@gmail.com>
2024-03-04 00:34:43 -0800

    implement method to take block and return running time

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,15 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+    
+    # Calculate the 200th Fibonacci number
+  yield
+    
+  end_time = Time.now
+    
+    # This will return the difference in the timestamps in seconds
+  running_time = end_time - start_time
+  return running_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
